128520274950ac9481d271075a6481cd20b8de0b
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-26 12:27:44 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordArray=word.split('')
+  let reversedArray=wordArray.reverse()
+  let reversedWord=reversedArray.join('')
+  if (reversedWord===word){
+    return true
+  }
+  else{
+    return false
+  }
+  
+
+
 }
 
 /* 
@@ -8,6 +20,13 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+
+  first split the word.
+  then  reverse the split word
+  After which you join the reverse splitted word
+   then check if the reversed word is equal to the word 
+   if its true or equal tthen return the word
+   if its not equal or false then return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,6 +36,9 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("ALI"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
